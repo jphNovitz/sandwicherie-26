a1c18848a401c642ffc0989aa152ef3180377c38
Novitz Jean-Philippe <novitz@gmail.com>
2026-03-27 13:08:21 +0100

    feat(contact): handle form submission

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
         $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Votre message a bien ete envoye.');
+
+            return $this->redirectToRoute('app_contact');
+        }
 
         return $this->render('contact/index.html.twig', [
             'form' => $form,
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,19 +5,50 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email', EmailType::class)
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez indiquer votre nom.',
+                    ]),
+                    new Length([
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez indiquer votre email.',
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez indiquer une adresse email valide.',
+                    ]),
+                ],
+            ])
             ->add('message', TextareaType::class, [
                 'attr' => [
                     'rows' => 6,
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un message.',
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Votre message doit contenir au moins {{ limit }} caracteres.',
+                    ]),
                 ],
             ])
         ;
