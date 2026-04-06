ad86ca33546994a1169264b9bb5a1a727c4f557f
Novitz Jean-Philippe <novitz@gmail.com>
2026-04-07 00:30:49 +0200

    feat(home): display featured items on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\FeaturedItem;
 use App\Enum\PageCode;
+use App\Enum\FeaturedItemType;
 use App\Entity\Page;
 use App\Entity\SiteSettings;
-use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProductRepository $productRepository, EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         /** @var Page|null $page */
         $page = $entityManager->getRepository(Page::class)->findOneBy([
@@ -23,9 +24,30 @@ final class HomeController extends AbstractController
         ]);
         /** @var SiteSettings|null $siteSettings */
         $siteSettings = $entityManager->getRepository(SiteSettings::class)->findOneBy([], ['id' => 'ASC']);
+        /** @var list<FeaturedItem> $featuredItems */
+        $featuredItems = null !== $siteSettings
+            ? $entityManager->getRepository(FeaturedItem::class)->findBy([
+                'siteSettings' => $siteSettings,
+                'isActive' => true,
+            ], [
+                'position' => 'ASC',
+                'id' => 'ASC',
+            ])
+            : [];
+
+        $featuredItems = array_values(array_filter(
+            $featuredItems,
+            static function (FeaturedItem $featuredItem): bool {
+                return match ($featuredItem->getType()) {
+                    FeaturedItemType::PRODUCT => $featuredItem->getProduct()?->isAvailable() ?? false,
+                    FeaturedItemType::CATEGORY => $featuredItem->getCategory()?->isActive() ?? false,
+                    default => false,
+                };
+            }
+        ));
 
         return $this->render('home/index.html.twig', [
-            'products' => $productRepository->findBy(['isAvailable' => true], ['id' => 'DESC']),
+            'featured_items' => $featuredItems,
             'page' => $page,
             'site_settings' => $siteSettings,
         ]);
